61bc795fd280ff9e4b40ea4250917a668780ce08
Christian <christianmichelmallma@gmail.com>
2023-12-04 19:48:41 -0500

    update admin panel administration => install django-admin-tailwind

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Category, Product
 
 # Register your models here.
+admin.site.register(Category)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'category', 'registration_date')
+    list_editable = ('price',)
